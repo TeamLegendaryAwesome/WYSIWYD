0b7719d11e044d729a5f1d31995e1f55f7a2ff52
Bjurbäck <mathias.bjurback@gmail.com>
2012-10-01 15:56:19 +0200

    Added Autocomplete

--- src/se/turbotorsk/mybar/Add_Ingredient_Activity.java
+++ src/se/turbotorsk/mybar/Add_Ingredient_Activity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.SearchView;
 
 public class Add_Ingredient_Activity extends Activity {
@@ -13,12 +15,21 @@ public class Add_Ingredient_Activity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_add_ingredient);
         
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+       /* SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) findViewById(R.id.searchView1);
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setIconifiedByDefault(false);
         
-       
+     // Get a reference to the AutoCompleteTextView in the layout
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.textView1);
+        // Get the string array
+        String[] ingredients = getResources().getStringArray(R.array.ingredients);
+        // Create the adapter and set it to the AutoCompleteTextView 
+        ArrayAdapter<String> adapter = 
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ingredients);
+        textView.setAdapter(adapter);
+        
+       */
     }
 
 }
