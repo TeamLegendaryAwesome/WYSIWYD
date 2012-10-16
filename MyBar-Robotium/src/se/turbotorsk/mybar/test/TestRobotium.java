345fcdf57a095cc7954ba608e202a49d16ac4fd4
Adam Clark <adam.clark91@gmail.com>
2012-10-16 23:47:19 +0200

    Added robotium project

---
+++ MyBar-Robotium/src/se/turbotorsk/mybar/test/TestRobotium.java
@@ -0,0 +1,26 @@
+package se.turbotorsk.mybar.test;
+
+import com.jayway.android.robotium.solo.Solo;
+import se.turbotorsk.mybar.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class TestRobotium extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+	
+	private Solo solo;
+
+	public TestRobotium() {
+		super("se.turbotorsk.mybar", MainActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
+	public void testStartApp(){
+		solo.assertCurrentActivity("Seems like its the wrong activity //Fungerar inte än", MainActivity.class);
+		//fail();
+	}
+
+}
